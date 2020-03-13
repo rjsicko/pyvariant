5050b3f36ecac9c28fe32afb804d1a057a97946c
mattdoug604 <mattdoug604@gmail.com>
2020-03-13 13:53:55 -0700

    update description to reflect scope

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="mattdoug604",
     author_email="mattdoug604@gmail.com",
     packages=find_packages(),
-    description="Map between gene, transcript, exon, and protein coordinates.",
+    description="Map between gene, transcript, exon, CDS, and protein coordinates.",
     url="",
     python_requires=">=3",
     install_requires=["pyensembl"],
