7e5c350b68da4628ee6e573a4ae3b313475a5e13
mattdoug604 <mattdoug604@gmail.com>
2020-03-13 08:53:01 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ensembl_map",
+    version="",
+    author="mattdoug604",
+    author_email="mattdoug604@gmail.com",
+    packages=find_packages(),
+    description="Map between gene, transcript, exon, and protein coordinates.",
+    url="",
+    python_requires=">=3",
+    install_requires=[],
+)
