950c6ed1cee16f8fe280563aeade65d2dd5d9a89
mattdoug604 <mattdoug604@gmail.com>
2020-03-13 13:57:51 -0700

    add URL to description

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="mattdoug604@gmail.com",
     packages=find_packages(),
     description="Map between gene, transcript, exon, CDS, and protein coordinates.",
-    url="",
+    url="https://github.com/mattdoug604/ensembl_map.git",
     python_requires=">=3",
     install_requires=["pyensembl"],
 )
