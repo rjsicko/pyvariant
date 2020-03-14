2d0fb4ffacbe0c3faaeb3fbce99c0198600f38d4
mattdoug604 <mattdoug604@gmail.com>
2020-03-13 22:28:56 -0700

    remove unused function

--- ensembl_map/util.py
+++ ensembl_map/util.py
@@ -5,11 +5,3 @@ def is_ensembl_id(feature):
     """The given string looks like and Ensembl ID."""
     return bool(re.match(r"ENS[A-Z]\d{8}", feature))
 
-
-def in_ranges(number, ranges):
-    """Assert that a number falls inside one or more ranges."""
-    for i, j in ranges:
-        if i <= number <= j:
-            return True
-    else:
-        return False
