923fa87a1f9d9f4094460196b51bd5227c0c4f98
mattdoug604 <mattdoug604@gmail.com>
2020-03-28 14:40:02 -0700

    merge config into cache

--- ensembl_map/cache.py
+++ ensembl_map/cache.py
@@ -10,6 +10,16 @@ DEFAULT_RELEASE = 99
 DEFAULT_SPECIES = "homo_sapiens"
 
 
+def set_cache_dir(path):
+    """Set the root directory where cache files are stored."""
+    Ensembl().set_cache_dir(path)
+
+
+def set_ensembl_release(release, species, cache_dir=None, download_if_missing=False):
+    """Set the Ensembl release to use."""
+    return Ensembl().load(release, species, cache_dir, download_if_missing)
+
+
 @singleton
 class Ensembl:
     """Container for a `pyensembl.EnsemblRelease` object.
