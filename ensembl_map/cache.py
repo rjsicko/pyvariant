b12d6fca72417757a50084935e1fe2b259cbbe35
mattdoug604 <mattdoug604@gmail.com>
2020-03-31 11:19:46 -0700

    use human as default species when setting release

--- ensembl_map/cache.py
+++ ensembl_map/cache.py
@@ -15,7 +15,7 @@ def set_cache_dir(path):
     Ensembl().set_cache_dir(path)
 
 
-def set_ensembl_release(release, species, cache_dir=None, download_if_missing=False):
+def set_ensembl_release(release, species=DEFAULT_SPECIES, cache_dir=None, download_if_missing=False):
     """Set the Ensembl release to use."""
     return Ensembl().load(release, species, cache_dir, download_if_missing)
 
