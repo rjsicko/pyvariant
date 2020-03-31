3e3ed0e49151c1e9603e06133d90e30e19f7cf5d
mattdoug604 <mattdoug604@gmail.com>
2020-03-31 15:40:26 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 from setuptools import setup, find_packages
 
+name = "ensembl_map"
+version = "1.0.0"
+
 setup(
-    name="ensembl_map",
-    version="1.0.0",
+    name=name,
+    version=version,
     author="mattdoug604",
     author_email="mattdoug604@gmail.com",
     packages=find_packages(),
     description="Convert between gene, transcript, exon, CDS, and protein coordinates using Ensembl.",
     url="https://github.com/mattdoug604/ensembl_map.git",
-    python_requires=">=3.3",
+    python_requires=">=3",
+    license="https://spdx.org/licenses/MIT.html",
+    entry_points={"console_scripts": [f"ensembl_map = {name}.shell:run"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
     install_requires=["pyensembl>=1.8.5"],
     tests_require=["mock", "nose"],
     test_suite="nose.collector",
