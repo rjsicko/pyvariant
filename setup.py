f24607f1e6bd7b4cebf3c3afa5eda4cbcffd368f
mattdoug604 <mattdoug604@gmail.com>
2020-03-31 11:35:25 -0700

    updated setup script for v1.0.0

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ensembl_map",
-    version="",
+    version="1.0.0",
     author="mattdoug604",
     author_email="mattdoug604@gmail.com",
     packages=find_packages(),
-    description="Map between gene, transcript, exon, CDS, and protein coordinates.",
+    description="Convert between gene, transcript, exon, CDS, and protein coordinates using Ensembl.",
     url="https://github.com/mattdoug604/ensembl_map.git",
-    python_requires=">=3",
+    python_requires=">=3.3",
     install_requires=["pyensembl>=1.8.5"],
-    test_requires=["nose"],
+    tests_require=["mock", "nose"],
+    test_suite="nose.collector",
 )
