ede27f678c7b9d5f083b43e61a26df91fd32945d
mdouglas <mdouglas@bcgsc.ca>
2023-05-30 12:49:15 -0700

    add variant classes as top-level import

--- pyvariant/__init__.py
+++ pyvariant/__init__.py
@@ -2,3 +2,37 @@
 chromosome, cDNA, gene, exon, protein, and RNA positions.
 """
 from .ensembl_release import EnsemblRelease
+from .variants import (
+    CdnaDeletion,
+    CdnaDelins,
+    CdnaDuplication,
+    CdnaFusion,
+    CdnaInsertion,
+    CdnaPosition,
+    CdnaSubstitution,
+    DnaDeletion,
+    DnaDelins,
+    DnaDuplication,
+    DnaFusion,
+    DnaInsertion,
+    DnaPosition,
+    DnaSubstitution,
+    ExonFusion,
+    ExonPosition,
+    ExonSmallVariant,
+    ProteinDeletion,
+    ProteinDelins,
+    ProteinDuplication,
+    ProteinFrameshift,
+    ProteinFusion,
+    ProteinInsertion,
+    ProteinPosition,
+    ProteinSubstitution,
+    RnaDeletion,
+    RnaDelins,
+    RnaDuplication,
+    RnaFusion,
+    RnaInsertion,
+    RnaPosition,
+    RnaSubstitution
+)
